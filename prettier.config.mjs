1ebb365fde20aedbcaf6169dbfef74c3bf6b5ff7
Jacob Cassidy <jacob@jacobcassidy.com>
2025-08-05 09:40:47 +0700

    Add type and printWidth option

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -1,11 +1,14 @@
 /**
  * Prettier configuration.
- * @see https://prettier.io/docs/en/configuration.html
+ * @see https://prettier.io/docs/configuration
+ * @type {import("prettier").Config}
  */
+
 import wpConfig from '@wordpress/prettier-config';
 
 const config = {
 	...wpConfig,
+	printWidth: 120,
 };
 
 export default config;
